05da5ec4ad831e42f15c3852b218442a84fcce35
Daniel Hayes <dan.rdhayes@gmail.com>
2021-06-18 19:47:49 -0400

    add skeleton theme and start from scratch

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: ['./_includes/**/*.html', './_layouts/**/*.html', './*.html'],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      colors: {
+        primary: '#39abf7',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
