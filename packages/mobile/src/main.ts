cc0657789b31d7505ee9abd4f9f64f2b9ae0c7ea
Daniel Hayes <dan.rdhayes@gmail.com>
2021-08-29 00:09:08 -0400

    add app icons for ios

--- packages/mobile/src/main.ts
+++ packages/mobile/src/main.ts
@@ -1,21 +1,15 @@
+import { SplashScreen } from '@capacitor/splash-screen';
 import App from './components/App.svelte';
 import '@today/shared/styles/global.css';
 
+// Hide the splash (you should do this on app launch)
+SplashScreen.hide();
 // import { StatusBar, Style } from '@capacitor/status-bar';
 
 // iOS only
-window.addEventListener('statusTap', function () {
-  console.log('statusbar tapped');
-});
-
-const hideStatusBar = async () => {
-  // TODO: determine if background color is light or dark
-  // await StatusBar.setStyle({ style: Style.Light });
-  // await StatusBar.hide();
-};
-
-// https://capacitorjs.com/docs/apis/status-bar
-hideStatusBar();
+// window.addEventListener('statusTap', function () {
+//   console.log('statusbar tapped');
+// });
 
 const app = new App({
   target: document.getElementById('app'),
